f56370c6051fb50b50e18bd81ed2bc3d5e739439
Coconut-X <144850519+Coconut-X@users.noreply.github.com>
2024-03-13 06:40:09 +0500

    Time: 0 ms (100.00%) | Memory: 7.2 MB (41.86%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
@@ -1,22 +1,9 @@
 class Solution {
 public:
     int pivotInteger(int n) {
-       if(n<1 ||  n>1000) return -1;
-       int pre=0,suff=0;
+     
        for(int i=1;i<=n;i++)
-       {
-            // pre=i*(i+1)/2;
-            // suff=n*(n+1)/2 -pre;
-            // if(pre==suff) return i;
-
-            // pre=i*(i+1)/2;
-            // suff=n*(n+1)/2 -pre;
-            if(i*(i+1)/2==n*(n+1)/2 - i*(i-1)/2) return i;
-            //we had sum from 1-i and 1-n -(1-i); on other side of equation it will cancel out 2
-
-            pre=0,suff=0;
-       }
-        
+            if(i*(i+1)==n*(n+1) - i*(i-1)) return i;
         return -1;
     }
 };
