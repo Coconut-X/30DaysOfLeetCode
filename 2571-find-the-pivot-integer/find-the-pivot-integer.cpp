b79112ac2ef8f3aa18cbaa9a88a6e33eac65d40c
Coconut-X <144850519+Coconut-X@users.noreply.github.com>
2024-03-13 06:48:29 +0500

    Time: 4 ms (35.26%) | Memory: 7.4 MB (15.68%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
@@ -3,7 +3,9 @@ public:
     int pivotInteger(int n) {
      
        for(int i=1;i<=n;i++)
-            if(i*(i+1)==n*(n+1) - i*(i-1)) return i;
+           // if(i*(i+1)==n*(n+1) - i*(i-1)) return i;
+            if(2*i*i==n*(n+1)) return i;
+
         return -1;
     }
 };
