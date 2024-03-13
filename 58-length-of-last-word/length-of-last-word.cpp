72c897d87580ad8ddb5504f26317b31afc3ee3f3
Coconut-X <144850519+Coconut-X@users.noreply.github.com>
2024-03-13 21:59:08 +0500

    Time: 4 ms (45.31%) | Memory: 7.6 MB (51.00%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int c=0;
+        int size=s.length();
+        bool start=false;
+        for(int i=size-1;i>=0;i--)
+        {
+            if(s[i]!=' ') start=true;
+            if(start)
+            {
+                if(s[i]==' ')return c;
+                c++;
+            }
+            
+        }return c;
+    }
+};
