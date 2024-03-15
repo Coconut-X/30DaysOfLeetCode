e4181e692e5b6d31618f6d6039c93dc4fa84af77
Coconut-X <144850519+Coconut-X@users.noreply.github.com>
2024-03-15 14:26:13 +0500

    Time: 2454 ms (5.01%) | Memory: 7 MB (51.24%) - LeetSync

--- 367-valid-perfect-square/valid-perfect-square.cpp
+++ 367-valid-perfect-square/valid-perfect-square.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     bool isPerfectSquare(int num) {
-        if(num==1) return true;
-        int half=num/2;
-        for(long int i=1;i<=half;i++)
+        //if(num==1) return true;
+        //int half=num/2;
+        for(long int i=1;i<=num;i++)
             if(i*i==num) return true;
         return false;    
     }
