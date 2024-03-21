9730ea981821f1430e6a0696b23317aa2ce0f602
Coconut-X <144850519+Coconut-X@users.noreply.github.com>
2024-03-21 14:16:13 +0500

    Time: 187 ms (5.04%) | Memory: 12.6 MB (67.42%) - LeetSync

--- 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.cpp
+++ 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.cpp
@@ -10,19 +10,21 @@ public:
                 int c1 = 0, c2 = 0; 
                 int num1 = arr[j];
                 int num2 = arr[j+1];
-                
-                while(num1 != 0) 
-                { 
-                    int rem = num1 % 2;
-                    num1 /= 2; 
-                    if(rem == 1) c1++;
-                }
-
-                while(num2 != 0)
-                {
-                    int rem = num2 % 2;
-                    num2 /= 2; 
-                    if(rem == 1) c2++;
+              
+                 while(num1 != 0 || num2 != 0) 
+                 { 
+                    if(num1 != 0) 
+                    {
+                        int rem = num1 % 2;
+                        num1 /= 2; 
+                        if(rem == 1) c1++;
+                    }
+                    if(num2 != 0) 
+                    {
+                        int rem = num2 % 2;
+                        num2 /= 2; 
+                        if(rem == 1) c2++;
+                    }
                 }
 
                 if(c1 > c2 || (c1 == c2 && arr[j] > arr[j+1])) 
