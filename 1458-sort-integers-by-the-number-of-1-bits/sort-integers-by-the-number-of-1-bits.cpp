4e0c0aeeb53833f9093bdf559884750f7e94d19f
Coconut-X <144850519+Coconut-X@users.noreply.github.com>
2024-03-21 14:20:44 +0500

    Time: 133 ms (5.04%) | Memory: 12.5 MB (67.42%) - LeetSync

--- 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.cpp
+++ 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.cpp
@@ -11,21 +11,22 @@ public:
                 int num1 = arr[j];
                 int num2 = arr[j+1];
               
-                 while(num1 != 0 || num2 != 0) 
+                 while(num1 != 0) 
                  { 
-                        int rem = num1 % 2;
-                        num1 /= 2; 
-                        if(rem == 1) c1++;
+                    int rem = num1 % 2;
+                    num1 /= 2; 
+                    if(rem == 1) c1++;
+                }
 
-                         rem = num2 % 2;
-                        num2 /= 2; 
-                        if(rem == 1) c2++;
+                while(num2 != 0) 
+                {
+                    int rem = num2 % 2;
+                    num2 /= 2; 
+                    if(rem == 1) c2++;
                 }
 
                 if(c1 > c2 || (c1 == c2 && arr[j] > arr[j+1])) 
-                { 
                     swap(arr[j], arr[j+1]);
-                }
             }
         }
         return arr;
