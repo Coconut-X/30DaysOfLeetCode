361b021cedb771c267a8098167d380a7fe49181c
Coconut-X <144850519+Coconut-X@users.noreply.github.com>
2024-03-28 07:42:52 +0500

    Time: 3 ms (49.42%) | Memory: 10.9 MB (72.36%) - LeetSync

---
+++ 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        int x=*max_element(candies.begin(),candies.end());
+        int s=candies.size();
+        vector<bool>result(s);
+
+        for(int i=0;i<s;i++)
+            {
+                if(extraCandies+candies[i]>=x) result[i]=true;
+                else result[i]=false;
+            }
+            return result;
+        
+    }
+};
